5cf09fce54644734582f1c64c47041109696cdb1
Mark Bastian <markbastian@gmail.com>
2015-04-10 22:29:33 -0600

    Swing half is working.

--- src/clj/mazegen/swingui.clj
+++ src/clj/mazegen/swingui.clj
@@ -1,60 +1,54 @@
-;(ns mazegen.swingui
-;  (:gen-class)
-;  (:require [mazegen.rules :as rules])
-;  (:import (javax.swing JFrame JPanel JButton BoxLayout Box)
-;           (java.awt BorderLayout Graphics2D Color ImageCapabilities)
-;           (java.awt.geom Rectangle2D$Double)
-;           (java.awt.event ActionListener)
-;           (java.awt.image VolatileImage)))
-;
-;(defn panel [grid]
-;  (doto (proxy [JPanel] []
-;    (paint [g]
-;      (let [w (.getWidth this)
-;            h (.getHeight this)
-;            bg (Rectangle2D$Double. 0 0 w (.getHeight this))
-;            vimg (.createVolatileImage this w h (ImageCapabilities. true))
-;            g2d (.createGraphics vimg)]
-;        (doto g2d
-;          (.setPaint Color/BLACK)
-;          (.fill bg))
-;        (doseq [i (range (count @grid))]
-;          (doseq [j (range (count (get @grid i)))]
-;            (let [c (if (= :alive (get-in @grid [i j])) Color/GREEN Color/RED)
-;                  sq (Rectangle2D$Double. (* i (/ w (count @grid)))
-;                                          (* j (/ h (count (get @grid i))))
-;                                          (/ w (count @grid))
-;                                          (/ h (count (get @grid i))))]
-;              (do (.setPaint g2d c) (.fill g2d sq)))))
-;        (.drawImage g vimg 0 0 this))))
-;    (#(add-watch grid :repaint (fn [_ _ _ _] (.repaint %))))))
-;
-;(defn sim [grid]
-;  (future (loop []
-;            (dosync (swap! grid rules/step))
-;            (recur))))
-;
-;(defn south-panel [grid-ref]
-;  (doto (Box. BoxLayout/Y_AXIS)
-;    (.add (doto (JButton. "Go")
-;            (.addActionListener
-;              (reify ActionListener
-;                (actionPerformed [_ _] (sim grid-ref))))))
-;    (.add (doto (JButton. "Reset")
-;            (.addActionListener
-;              (reify ActionListener
-;                (actionPerformed [_ _] (reset! grid-ref (rules/seed-grid 100 100)))))))))
-;
-;(defn launch [exit-behavior]
-;  (let [grid (atom (rules/seed-grid 100 100))]
-;    (doto (JFrame. "Maze Generator")
-;      (.setSize 800 600)
-;      (.setDefaultCloseOperation exit-behavior)
-;      (.add (panel grid) BorderLayout/CENTER)
-;      (.add (south-panel grid) BorderLayout/SOUTH)
-;      (.setVisible true))))
-;
-;(defn -main [& args]
-;  (launch JFrame/EXIT_ON_CLOSE))
-;
-;;(launch JFrame/DISPOSE_ON_CLOSE)
+(ns mazegen.swingui
+  (:gen-class)
+  (:require [mazegen.rules :as rules])
+  (:import (javax.swing JFrame JPanel JButton BoxLayout Box)
+           (java.awt BorderLayout Graphics2D Color ImageCapabilities)
+           (java.awt.geom Rectangle2D$Double Path2D$Double Ellipse2D$Double)))
+
+(defn draw-cell [p dx dy row col maze]
+  (let [sx (* dx col)
+        sy (* dy row)
+        cell (get-in maze [row col])]
+    (do
+      (when (cell :start)
+        (.append p (Ellipse2D$Double. sx sy dx dy) false))
+      (when-not (cell [row (dec col)])
+        (doto p (.moveTo sx sy) (.lineTo sx (+ sy dy))))
+      (when-not (cell [row (inc col)])
+        (doto p (.moveTo (+ sx dx) sy) (.lineTo (+ sx dx) (+ sy dy))))
+      (when-not (cell [(dec row) col])
+        (doto p (.moveTo sx sy) (.lineTo (+ sx dx) sy)))
+      (when-not (cell [(inc row) col])
+        (doto p (.moveTo sx (+ sy dy)) (.lineTo (+ sx dx) (+ sy dy)))))))
+
+(defn panel [maze]
+  (doto (proxy [JPanel] []
+          (paint [g2d]
+            (let [dx (/ (.getWidth this) (count (maze 0)))
+                  dy (/ (.getHeight this) (count maze))
+                  p (Path2D$Double.)
+                  bg (Rectangle2D$Double. 0 0 (.getWidth this) (.getHeight this))]
+              (do
+                (doseq [row (range (count maze))]
+                  (doseq [col (range (count (get maze row)))]
+                    (draw-cell p dx dy row col maze)))
+                (doto g2d
+                  (.setPaint Color/WHITE)
+                  (.fill bg)
+                  (.setPaint Color/BLACK)
+                  (.draw p))))))))
+
+(defn launch [exit-behavior]
+  (let [cells 20
+        empty-maze (rules/create-empty cells cells)
+        prim-maze (rules/prim-gen empty-maze [0 0])]
+    (doto (JFrame. "Maze Generator")
+      (.setSize 800 600)
+      (.setDefaultCloseOperation exit-behavior)
+      (.add (panel prim-maze) BorderLayout/CENTER)
+      (.setVisible true))))
+
+(defn -main [& args]
+  (launch JFrame/EXIT_ON_CLOSE))
+
+(launch JFrame/DISPOSE_ON_CLOSE)
